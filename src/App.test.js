be86063a18a3c5fd6d20ee173f1b016f3ccd8910
Eduardo Guette <eduardoguette@github.com>
2020-08-22 18:00:34 +0200

    add eslint prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { render, fireEvent, screen, test, expect } from '@testing-library/react'
-import App from './App'
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  screen,
+  test,
+  expect,
+} from '@testing-library/react';
+import App from './App';
 
 test('renders without crashing', () => {
-  const { getByText } = render(<App />)
-  const title = getByText(/login/i)
-  expect(title).toBeInTheDocument()
-})
+  const { getByText } = render(<App />);
+  const title = getByText(/login/i);
+  expect(title).toBeInTheDocument();
+});
 
 test('login form', async () => {
-  render(<App />)
-  const input = await screen.findByRole('textbox')
-  const button = await screen.findByRole('button')
-  fireEvent.change(input, { target: { value: 'eve.holt@reqres.in' } })
-  fireEvent.click(button)
-})
+  render(<App />);
+  const input = await screen.findByRole('textbox');
+  const button = await screen.findByRole('button');
+  fireEvent.change(input, { target: { value: 'eve.holt@reqres.in' } });
+  fireEvent.click(button);
+});
